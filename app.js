198f0f59cbb4a5e3c43fe2c953db8ae1105b4dc6
Kyle Richmond <kylerichmond79@gmail.com>
2017-06-26 14:57:32 -0700

    inital work on app.js mob drive

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// data holding
+var toptilearray = [img1, img2, img3];
+var midtileArray = [img4, img5, img6];
+var bottileArray = [img7, img8, blankTile];
+var allArrays = [toptileArray, midtileArray, bottileArray];
+var totalMoves = 0;
+
+//function to render the tiles
