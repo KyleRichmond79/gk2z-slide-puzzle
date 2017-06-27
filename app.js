8b6a82bf12fb7d1f90b2e5cb99bca1a600a0129a
ZacharyJohnson <zaj6827@gmail.com>
2017-06-27 13:37:14 -0700

    changed canvas to render from arrays

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 'use strict';
 
 // data holding
-var toptileArray = [];
-var midtileArray = [];
-var bottileArray = [];
-var allTiles = [toptileArray, midtileArray, bottileArray];
 var tiles = document.getElementsByClassName('tiles');
 var totalMoves = 0;
 var tile1 = document.getElementById('Tile1');
@@ -16,21 +12,58 @@ var tile6 = document.getElementById('Tile6');
 var tile7 = document.getElementById('Tile7');
 var tile8 = document.getElementById('Tile8');
 var tile9 = document.getElementById('Tile9');
+var toptileArray = [tile1, tile2, tile3];
+var midtileArray = [tile4, tile5, tile6];
+var bottileArray = [tile7, tile8, tile9];
+var allTiles = [toptileArray, midtileArray, bottileArray];
+var allImages = [];
+
+
 
 function makeChart() {
   var gameCanvas = document.getElementById('gameCanvas')
-  var fullPic = document.getElementById('fullCatImage');
   var ctx = gameCanvas.getContext('2d');
-ctx.drawImage(tile1, 0, 0);
-ctx.drawImage(tile2, 180, 0);
-ctx.drawImage(tile3, 360, 0);
-ctx.drawImage(tile4, 0, 180);
-ctx.drawImage(tile5, 180, 180);
-ctx.drawImage(tile6, 360, 180);
-ctx.drawImage(tile7, 0, 360);
-ctx.drawImage(tile8, 180, 360);
-ctx.drawImage(tile9, 360, 360);
+ctx.drawImage(allTiles[0][0], 0, 0);
+ctx.drawImage(allTiles[0][1], 180, 0);
+ctx.drawImage(allTiles[0][2], 360, 0);
+ctx.drawImage(allTiles[1][0], 0, 180);
+ctx.drawImage(allTiles[1][1], 180, 180);
+ctx.drawImage(allTiles[1][2], 360, 180);
+ctx.drawImage(allTiles[2][0], 0, 360);
+ctx.drawImage(allTiles[2][1], 180, 360);
+ctx.drawImage(allTiles[2][2], 360, 360);
 };
+
+
+// Initial Tile Set up
+// function MakeComponents(width, height, path, xPos, yPos, type) {
+//   if (type === 'image') {
+//     this.image = new Image();
+//     this.image.src = path;
+//   }
+//     this.width = width;
+//     this.height = height;
+//     this.xPos = xPos;
+//     this.yPos = yPos;
+//     this.type = type;
+//
+//     this.update = function() {
+//       ctx = gameCanvas.getContext('2d');
+//       if (type === 'image') {
+//         ctx.drawImage(this.image,
+//             this.xPos,
+//             this.yPos,
+//             this.width,
+//             this.height);
+//         }
+//     }
+// }
+
+//function to render new set up based on array positions
+//
+//
+
+
 
 
 // function renderTiles() {
